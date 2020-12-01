6ee9dbee7b65ad0fde095b532dd3bbe6f5f82142
Colin Reeder <colin.c.reeder@gmail.com>
2020-11-30 23:10:37 -0700

    final

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const authCtrl = require('./authController');
+const treasureCtrl = require('./treasureController');
+const auth = require('./authMiddleware');
+
+
+app.post('/auth/register', authCtrl.register);
+app.post('/auth/login', authCtrl.login);
+app.get('/auth/logout', authCtrl.logout);
+
+app.get('/api/treasure/dragon', treasureCtrl.dragonTreasure);
+app.get('/api/treasure/user', auth.usersOnly, treasureCtrl.getUserTreasure);
+app.post('/api/treasure/user', auth.usersOnly, treasureCtrl.addUserTreasure);
+app.get('/api/treasure/all', auth.usersOnly, auth.adminsOnly, treasureCtrl.getAllTreasure)
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
